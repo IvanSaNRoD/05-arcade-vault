51a742e089ef702be6ca5c84ac3affb978b0daa8
Ivan_Sanchez <ivancsancrodr@gmail.com>
2026-09-16 16:42:36 +0200

    feat: games page & route added

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,3 @@
-import { Library } from "@/components/library";
-
 export default function Home() {
-  return (
-    <div className="fade-in">
-      <section className="av-hero">
-        <h1 className="flicker">ARCADE VAULT</h1>
-        <div className="sub">
-          INSERTA UNA MONEDA PARA JUGAR <span className="blink">_</span>
-        </div>
-      </section>
-      <Library />
-    </div>
-  );
+  return <div className="fade-in" />;
 }
